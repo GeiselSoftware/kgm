cb779e4227b82fbd7206658ae9d496ac581c82d7
Andrei Smirnov <andrei.smirnov.69@gmail.com>
2024-07-18 23:20:18 -0400

    combobox in node does not work - we need to investigate and fix the problem.

--- apps/shacled/visnode-userclass.h
+++ apps/shacled/visnode-userclass.h
@@ -17,8 +17,15 @@ public:
   struct Member {
     bool checkbox_value = false;
     std::string member_name, member_type;
+    bool is_member_type_dataclass = true; // sh:class or sh:dataclass
     ax::NodeEditor::PinId out_pin_id;
-    
+
+#if 0 // testing of combobox
+    std::vector<std::string> member_types = { "option 1", "option 2", "option 3" };
+    const char* get_member_type_at(int idx) { return member_types[idx].c_str(); }
+    int combo_selected_index = 1;
+#endif
+
     Member();
     Member(const std::string& member_name, const std::string& member_type);
   };
--- libs/lib-utils/known-prefixes.h
+++ libs/lib-utils/known-prefixes.h
@@ -3,12 +3,44 @@
 #include <lib-utils/rdf-utils.h>
 
 struct rdf {
-  static inline URI type{"http://www.w3.org/1999/02/22-rdf-syntax-ns#type"};
+  static inline URI __prefix{"http://www.w3.org/1999/02/22-rdf-syntax-ns#"};
+  static inline URI type{__prefix.uri + "type"};
 };
 
 struct rdfs {
-  static inline URI Class{"http://www.w3.org/2000/01/rdf-schema#Class"};
+  static inline URI __prefix{"http://www.w3.org/2000/01/rdf-schema#"};
+  static inline URI Class{__prefix.uri + "Class"};
 };
+
+struct xsd {
+  // using https://www.w3.org/TR/xmlschema-2/#built-in-primitive-datatypes
+  static inline URI __prefix{"http://www.w3.org/2001/XMLSchema#"};
+  
+  static inline URI string{__prefix.uri + "string"};
+  static inline URI boolean{__prefix.uri + "boolean"};
+  static inline URI decimal{__prefix.uri + "decimal"};
+  static inline URI float_{__prefix.uri + "float"};
+  static inline URI double_{__prefix.uri + "double"};
+  static inline URI duration{__prefix.uri + "duration"};
+  static inline URI dateTime{__prefix.uri + "dateTime"};
+  static inline URI time{__prefix.uri + "time"};
+  static inline URI date{__prefix.uri + "date"};
+  static inline URI gYearMonth{__prefix.uri + "gYearMonth"};
+  static inline URI gYear{__prefix.uri + "gYear"};
+  static inline URI gMonthDay{__prefix.uri + "gMonthDay"};
+  static inline URI gDay{__prefix.uri + "gDay"};
+  static inline URI gMonth{__prefix.uri + "gMonth"};
+  static inline URI hexBinary{__prefix.uri + "hexBinary"};
+  static inline URI base64Binary{__prefix.uri + "base64Binary"};
+
+  // from https://www.w3.org/TR/xmlschema-2/#built-in-derived
+  static inline URI integer{__prefix.uri + "integer"};
+  static inline URI long_{__prefix.uri + "long"};
+  static inline URI int_{__prefix.uri + "int"};
+  static inline URI short_{__prefix.uri + "short"};
+  static inline URI byte_{__prefix.uri + "byte"};
+};
+
 
 struct sh {
   static inline URI __prefix{"http://www.w3.org/ns/shacl#"};
