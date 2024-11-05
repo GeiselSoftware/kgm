22713881fdd31d16a438e4d0660daab148cdd8e4
Andrei Smirnov <andrei.smirnov.69@gmail.com>
2024-11-04 20:58:20 -0500

    introducted kgm-editor - electron ui package with pip support

--- py-packages/kgm-editor/build-electron-distrib.py
+++ py-packages/kgm-editor/build-electron-distrib.py
@@ -1,31 +1,33 @@
 """
-cd build.wasm/electron-dist
+cd electron-dist
 aws s3 cp kgm-editor--linux-x64.zip s3://yaooas69-scratch
 aws s3 cp kgm-editor--win32-x64.zip s3://yaooas69-scratch
 aws s3 cp kgm-editor--darwin-arm64.zip s3://yaooas69-scratch
 """
 
-import os
+import os, subprocess, shlex, shutil
 
-def make_distrib(dist):
+def make_distrib(top_dir, top_dist_dir, dist):
     electron_distrib_url = f"https://github.com/electron/electron/releases/download/v31.7.0/electron-v31.7.0-{dist}.zip"
-    dist_dir = os.path.realpath("build.wasm/electron-dist/" + dist)
+    dist_dir = os.path.join(top_dist_dir, dist)
     
     if 1:
         if os.path.exists(dist_dir):
-            raise Exception(f"distrib dir {dist_dir} exists, giving up")
+            shutil.rmtree(dist_dir)
         os.makedirs(dist_dir)
 
-    cmd = f"cd {dist_dir} && wget {electron_distrib_url}"
+    cmd = f"cd {dist_dir} && wget -q {electron_distrib_url}"
     if 1:
-        print(cmd)
+        print("CMD:", cmd)
         os.system(cmd)
+        print("----------------")
 
     if 1:
         zip_fn = os.path.basename(electron_distrib_url)
         cmd = f"cd {dist_dir} && unzip {zip_fn} && rm {zip_fn}"
-        print(cmd)
+        print("CMD:", cmd)
         os.system(cmd)
+        print("----------------")
 
     cmds = []
     if dist == "linux-x64":
@@ -43,19 +45,28 @@ def make_distrib(dist):
     else:
         raise Exception(f"unknown dist {dist}")
 
-    cmds.append(f"cd apps/shacled-electron && cp main.js package.json {electron_app_dir}")
-    cmds.append(f"cp build.wasm/apps/shacled/run-shacled.* {electron_app_dir}")
+    cmds.append(f"cd {top_dir}/apps/shacled-electron && cp main.js package.json {electron_app_dir}")
+    cmds.append(f"cp {top_dir}/build.wasm/apps/shacled/run-shacled.* {electron_app_dir}")
     for cmd in cmds:
-        print(cmd)
+        print("CMD:", cmd)
         os.system(cmd)
+        print("----------------")
     
 
 if __name__ == "__main__":
-    for dist in ["linux-x64", "win32-x64", "darwin-arm64"]:
-        make_distrib(dist)
-        os.system(f"cd build.wasm/electron-dist && mv {dist} kgm-editor--{dist}")
-        os.system(f"cd build.wasm/electron-dist && zip -r kgm-editor--{dist}.zip kgm-editor--{dist}")
-        os.system(f"cd build.wasm/electron-dist && rm -rf kgm-editor--{dist}")
+    cmd = "git rev-parse --show-toplevel"    
+    top_dir = subprocess.check_output(shlex.split(cmd)).decode('ascii').strip()
+    print(f"top_dir: {top_dir}")
+
+    top_dist_dir = os.path.realpath(f"{top_dir}/py-packages/kgm-editor/kgm_editor/electron-dist/")
+    print(f"top_dist_dir: {top_dist_dir}")
+    
+    supported_dists = ["linux-x64", "win32-x64", "darwin-arm64"]
+    for dist in supported_dists:
+        make_distrib(top_dir, top_dist_dir, dist)
+        #os.system(f"cd electron-dist && mv {dist} kgm-editor--{dist}")
+        #os.system(f"cd electron-dist && zip -r kgm-editor--{dist}.zip kgm-editor--{dist}")
+        #os.system(f"cd electron-dist && rm -rf kgm-editor--{dist}")
 
     print("all done.")
     
---
+++ py-packages/kgm-editor/kgm_editor/__main__.py
@@ -0,0 +1,5 @@
+def main():
+    print("Hello")
+    # linux: 
+    # mac: ./kgm-editor.app/Contents/MacOS/Electron -- http://localhost:3030/kgm-default-dataset/query /gimp-test.shacl
+    
