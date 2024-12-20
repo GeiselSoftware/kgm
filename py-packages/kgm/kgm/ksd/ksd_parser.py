38f6f12721faa2ef337528daab96b15c82d4fb10
Andrei Smirnov <asmirnov@Andreis-MacBook-Air.local>
2024-12-20 01:27:01 -0500

    ksd parser wip

--- py-packages/kgm/kgm/ksd/ksd_parser.py
+++ py-packages/kgm/kgm/ksd/ksd_parser.py
@@ -1,4 +1,5 @@
-from lark import Lark, Transformer
+import ipdb
+from lark import Lark, Visitor
 
 ksd_grammar = \
     """
@@ -15,51 +16,90 @@ ksd_grammar = \
     class_member_type: curie
     class_member_cardinality: cardinality_spec
     
-    curie_prefix: CNAME? ":"
-    curie:  CNAME? ":" CNAME
+    curie_prefix: CNAME? COLON
+    curie: CNAME? COLON CNAME
+    COLON: ":"
     uri: /https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)/
-    cardinality_spec: "[" number ".." number "]"
-    number: DIGIT+ | "n" | "inf"
-    string : ESCAPED_STRING
+    cardinality_spec: "[" minc ".." maxc "]"
+    minc: INT
+    maxc: INT | INF
+    INF: "inf" | "n"
     
     %import common.CNAME
-    %import common.DIGIT
-    %import common.ESCAPED_STRING
+    %import common.INT
     %import common.SH_COMMENT
-    %import common.SIGNED_NUMBER
     %import common.WS
 
     %ignore SH_COMMENT
     %ignore WS
     """
 
-class MyTransformer(Transformer):
-    def prefix(self, v):
-        print("prefix:", v)
-        #return v
-        
-    def class_uri(self, v):
-        print("class_uri:", v)
-
-    def subclass_uri(self, v):
-        print("subclass_uri:", v)
+class MyClassMemberVisitor(Visitor):
+    def __init__(self):
+        self.min_c = 1
+        self.max_c = 1
         
     def class_member_name(self, m):
-        print("class_member_name:", m)
-
+        self.member_name_uri = "".join([x.value for x in m.children[0].children])
+        #print("class_member_name:", member_name_uri)
+        
     def class_member_type(self, m):
-        print("class_member_type:", m)
+        self.member_type_uri = "".join([x.value for x in m.children[0].children])
+        #print("class_member_type:", member_type_uri)
 
     def class_member_cardinality(self, m):
-        print("class_member_cardinality:", m)
+        minc = m.children[0].children[0].children[0].value
+        maxc = m.children[0].children[1].children[0].value
+        self.min_c = int(minc)
+        self.max_c = int(maxc) if maxc not in ["n", "inf"] else -1
+        #ipdb.set_trace()
+        #print("class_member_cardinality:", minc, maxc)
         
-    #def __default_token__(self, t):
-    #    print(t)
+class MyClassVisitor(Visitor):
+    def __init__(self):
+        self.members = []
+        self.superclasses = []
 
+    def subclass_uri(self, m):
+        #print("subclass_uri:", m)
+        self.superclasses.append("".join([x.value for x in m.children[0].children]))
+        #ipdb.set_trace()
+        
+    def class_member(self, m):
+        #print("class_member:", m)
+        v = MyClassMemberVisitor()
+        v.visit(m)
+        self.members.append((v.member_type_uri, v.member_name_uri, v.min_c, v.max_c))
+    
+class MyVisitor(Visitor):
+    def __init__(self):
+        self.local_prefixes = {}
+        self.known_rdfs_classes = {}
+        
+    def prefix(self, m):
+        prefix = "".join([x.value for x in m.children[0].children])
+        uri = m.children[1].children[0].value
+        self.local_prefixes[prefix] = uri
+
+    def rdfs_class_def(self, m):
+        class_uri = "".join([x.value for x in m.children[0].children[0].children])
+        if class_uri in self.known_rdfs_classes:
+            raise Exception(f"attempt to redefined rdfs class {class_uri}")
+        v = MyClassVisitor(); v.visit(m)
+        self.known_rdfs_classes[class_uri] = (v.superclasses, v.members)
+
+    def rdfs_class_ext(self, m):
+        class_uri = "".join([x.value for x in m.children[0].children[0].children])
+        if not class_uri in self.known_rdfs_classes:
+            raise Exception(f"attempt to extend unknown rdfs class {class_uri}")        
+        v = MyClassVisitor(); v.visit(m)
+        self.known_rdfs_classes[class_uri][0].extend(v.superclasses)
+        self.known_rdfs_classes[class_uri][1].extend(v.members)
+        
 class KSDParser:
     def do_it(self, kgm_path, ksd_filename):
-        print("ksd_filename:", ksd_filename, type(ksd_filename))
-        print("kgm_path:", kgm_path)
+        #print("ksd_filename:", ksd_filename, type(ksd_filename))
+        #print("kgm_path:", kgm_path)
         
         l = Lark(ksd_grammar)
         with open(ksd_filename, 'r') as f:
@@ -69,4 +109,16 @@ class KSDParser:
 
         tree = l.parse(ksd_code, start = "start")
         #print(tree.pretty())
-        MyTransformer().transform(tree)
+
+        v = MyVisitor()
+        v.visit_topdown(tree)
+        print("+++++++++++++++")
+        for prefix, prefix_uri in v.local_prefixes.items():
+            print(prefix, prefix_uri)
+
+        print("---------------")
+        for rdfs_class_uri, cls in v.known_rdfs_classes.items():
+            print(rdfs_class_uri, cls[0]) # superclasses
+            print("  ", cls[1]) # members
+            
+        
