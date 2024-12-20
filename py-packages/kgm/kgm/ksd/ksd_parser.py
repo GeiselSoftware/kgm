35227bd51234968f7af675ce1accd38413e7a460
Andrei Smirnov <asmirnov@Andreis-MacBook-Air.local>
2024-12-19 19:11:40 -0500

    lark grammar of ksd implemented: wip

--- py-packages/kgm/kgm/ksd/ksd_parser.py
+++ py-packages/kgm/kgm/ksd/ksd_parser.py
@@ -2,9 +2,36 @@ from lark import Lark
 
 ksd_grammar = \
     """
-    start: WORD "," WORD "!"
-    %import common.WORD   // imports from terminal library
-    %ignore " "           // Disregard spaces in text
+    ?start: value
+
+    value: prefixes class_defs
+
+    prefixes : ("prefix" curie_prefix "<" uri ">")*
+    
+    class_defs: rdfs_class_def (rdfs_class_def | rdfs_class_ext)*
+    rdfs_class_def : "class" class_uri ("subclass of" subclass_uri)? ("end" | class_body "end")
+    rdfs_class_ext : "extend class" class_uri ("subclass of" subclass_uri)? ("end" | class_body "end")
+    class_uri: curie
+    subclass_uri: curie
+    class_body: class_member (WS class_member)*
+    class_member: curie curie cardinality_spec?
+
+    curie_prefix: CNAME? ":"
+    curie:  CNAME? ":" CNAME
+    uri: /https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)/
+    cardinality_spec: "[" number ".." number "]"
+    number: DIGIT+ | "n" | "inf"
+    string : ESCAPED_STRING
+    
+    %import common.CNAME
+    %import common.DIGIT
+    %import common.ESCAPED_STRING
+    %import common.SH_COMMENT
+    %import common.SIGNED_NUMBER
+    %import common.WS
+
+    %ignore SH_COMMENT
+    %ignore WS
     """
 
 class KSDParser:
@@ -18,4 +45,6 @@ class KSDParser:
 
         #print(ksd_code)
         #print(l.parse("Hello, world!"))
-        print(l.parse(ksd_code))
+        #print(l.parse(ksd_code))
+        tree = l.parse(ksd_code)
+        print(tree.pretty())
